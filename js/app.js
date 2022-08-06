129bb348dc88a2029637066bd8c0ec2805791278
valentin-morice <valentinmorice1@gmail.com>
2022-08-06 23:59:41 +0200

    chain URL variables

--- js/app.js
+++ js/app.js
@@ -1,77 +1,58 @@
 // Listen to Subcategory Select on category.php
 let subcategory;
 $("#selectSubcategories").change(function () {
-  subcategory = $(this).val();
-  console.log(subcategory);
-  if (subcategory) {
-    $("#searchSubcategories").on("click", function () {
-      window.location.href =
-        document.location.origin + "/category/" + subcategory;
-    });
-  }
+    subcategory = $(this).val();
+    console.log(subcategory);
+    if (subcategory) {
+        $("#searchSubcategories").on("click", function () {
+            window.location.href =
+                document.location.origin + "/category/" + subcategory;
+        });
+    }
 });
 
 // Dynamically Populate Subcategory Dropdown in home.php
 $("#selectCategoriesHome").change(function () {
-  $("#selectSubcategoriesHome")
-    .empty()
-    .append('<option value="">--Please choose an option--</option>"');
-  cat_ID = $(this).val();
-  $.get(
-    "http://jurrien.local/wp-json/wp/v2/categories?parent=" + cat_ID,
-    function (data) {
-      for (i = 0; i < Object.keys(data).length; i++) {
-        $("#selectSubcategoriesHome").append(
-          '<option value="' +
-            data[i]["slug"] +
-            '">' +
-            data[i]["name"] +
-            "</option>"
-        );
-      }
-    }
-  );
+    $("#selectSubcategoriesHome")
+        .empty()
+        .append('<option value="">--Please choose an option--</option>"');
+    let cat_ID = $(this).val();
+    $.get(
+        "http://jurrien.local/wp-json/wp/v2/categories?parent=" + cat_ID,
+        function (data) {
+            for (i = 0; i < Object.keys(data).length; i++) {
+                $("#selectSubcategoriesHome").append(
+                    '<option value="' +
+                    data[i]["id"] +
+                    '">' +
+                    data[i]["name"] +
+                    "</option>"
+                );
+            }
+        }
+    );
 });
 
 // Activate Button Conditionally on home.php
 $("#searchHome").on("click", function () {
-  // If Parent Category is Selected But No Subcategory
-  if (
-    $("#selectCategoriesHome").val() &&
-    !$("#selectSubcategoriesHome").val()
-  ) {
-    $.get(
-      "http://jurrien.local/wp-json/wp/v2/categories?include=" +
-        $("#selectCategoriesHome").val(),
-      function (data) {
+
+    // Sets Category or Subcategory ID in URL Query
+    const category_id = $("#selectSubcategoriesHome").val() ? $("#selectSubcategoriesHome").val() : $("#selectCategoriesHome").val();
+
+    // Build URL Query
+    if ($("#selectCategoriesHome").val() || $("#selectSubcategoriesHome").val() || $("#century").val()) {
         window.location.href =
-          document.location.origin + "/category/" + data[0]["slug"];
-      }
-    );
-  }
-
-  // If Parent and Subcategory are Selected
-  if ($("#selectCategoriesHome").val() && $("#selectSubcategoriesHome").val()) {
-    window.location.href =
-      document.location.origin +
-      "/category/" +
-      $("#selectSubcategoriesHome").val();
-  }
-
-  if ($("#century").val()) {
-    window.location.href =
-        document.location.origin +
-        "?century=" +
-        $("#century").val();
-  }
+            document.location.origin +
+            "?cat=" + category_id + "&century=" + $("#century").val();
+    }
 });
 
-/* Set the width of the side navigation to 250px */
+/* Sets the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "250px";
 }
 
-/* Set the width of the side navigation to 0 */
+/* Sets the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0";
 }
