fcaf7e3ec5c874a29f9b9a3d3ad40ef2be9ecada
valentin-morice <valentinmorice1@gmail.com>
2022-08-14 15:25:45 +0200

    modify styling on filter form (sub-categories)

--- template_parts/searchCategories.php
+++ template_parts/searchCategories.php
@@ -1,6 +1,4 @@
-<p><a href="/catalogue"><< Back to Catalogue</a></p>
-<p>You are browsing the parent category "<?php echo single_cat_title(); ?>".
-      <br/>To filter the results further, choose a subcategory:</p>
-        <div class="d-flex flex-lg-row flex-column align-items-lg-center align-items-start">
-            <select name="subcategories" id="selectSubcategories" class="p-2 ">
-                <option value="">--Please choose an option--</option>
+<p>You are browsing the parent category "<?php echo single_cat_title(); ?>". To filter the results further, choose a subcategory:</p>
+<div class="d-flex flex-lg-row flex-column align-items-lg-center align-items-stretch">
+    <select name="subcategories" id="selectSubcategories" class="p-2 ">
+        <option value="">--Choose an option--</option>
--- template_parts/searchEnd.php
+++ template_parts/searchEnd.php
@@ -1,3 +1,6 @@
 </select>
-<button class="btn btn-primary text-white mt-2 mt-lg-0 ms-lg-2 mb-5 mb-lg-0" id="searchSubcategories">Search</button>
+<button class="btn btn-primary text-white mt-2 mt-lg-0 ms-lg-2 mb-4 mb-lg-0" id="searchSubcategories">Search</button>
+</div>
+<div class="d-flex flex-lg-row flex-column align-items-lg-end align-items-lg-start align-items-stretch py-4 pt-3 px-4 border rounded mt-xxl-4">
+    <p style='margin-bottom: -4px'><a href="/catalogue">< Back to Catalogue</a></p>
 </div>
